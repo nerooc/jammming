b00455951cecfbff51150567ad62d718d94db196
nerooc <tomaszgajdait@gmail.com>
2020-07-19 10:11:07 +0200

    updating the state of SearchResults and App, making TrackList universal

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,19 +4,31 @@ import  SearchBar  from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
-function App() {
+
+class App extends React.Component {
+
+    constructor(props){
+      super(props);
+
+      this.state = {
+        searchResults: [name, artist, album, id]
+      }
+    }
+
+    render(){
     return (
         <div>
             <h1>Ja<span className="highlight">mmm</span>ing</h1>
             <div className="App">
               <SearchBar />
                 <div className="App-playlist">
-                  <SearchResults />
+                  <SearchResults searchResults = {this.state.searchResults} />
                   <Playlist />
                 </div>
             </div>
         </div>
     );
+    }
 }
 
 export default App;
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -6,8 +6,15 @@ class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-                <Track track = {{name: "test", artist: "test", album:"test"}} />
-                <Track track = {{name: "test", artist: "test", album:"test"}} />
+                {
+          this.props.tracks.map(track => {
+            return <Track track={track}
+                          key={track.id}
+                          onAdd={this.props.onAdd}
+                          isRemoval={this.props.isRemoval}
+                          onRemove={this.props.onRemove} />
+          })
+        }
             </div>
         )
     }
